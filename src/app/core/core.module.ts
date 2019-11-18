46c26aadd0aaf76f511ec37241939b4b6547a479
Alin Salaru <mplacintasalaru@deloittece.com>
2019-11-18 21:47:07 +0200

    add components

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoreComponent } from './core.component';
+import { ContentComponent } from './content/content.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [CoreComponent],
+  declarations: [CoreComponent, ContentComponent, HeaderComponent],
   imports: [
     CommonModule
   ],
